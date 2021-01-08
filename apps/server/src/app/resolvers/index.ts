61f310b7d344e642c2b7cd11210dc246543d07f7
ian13456 <21ianh1@students.tas.tw>
2021-01-08 19:27:10 +0800

    feat: eslint and server setup

---
+++ apps/server/src/app/resolvers/index.ts
@@ -0,0 +1,9 @@
+import Query from './Query';
+// import Mutation from './Mutation';
+// import Subscription from './Subscription';
+
+export default {
+  Query,
+  // Mutation,
+  // Subscription,
+};
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import resolvers from './app/resolvers';
+
+import { ApolloServer } from 'apollo-server';
+
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const server = new ApolloServer({
+  typeDefs: readFileSync(
+    join(__dirname, 'assets/schemas/schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}.`));
