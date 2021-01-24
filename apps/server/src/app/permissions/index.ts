b223312d22c5dd75984e99537fd2ea0e45b122f5
ian13456 <21ianh1@students.tas.tw>
2021-01-25 02:23:45 +0800

    fix: tracker query permissions

--- apps/server/src/app/permissions/index.ts
+++ apps/server/src/app/permissions/index.ts
@@ -3,21 +3,21 @@ import { getUserId } from '../utils';
 import { shield, rule, allow, deny } from 'graphql-shield';
 
 const rules = {
-  isAuthenticatedUser: rule({ cache: 'contextual' })(
-    (parent, args, context) => {
-      const userId = getUserId(context);
-      return Boolean(userId);
-    }
-  ),
+  isAuthenticated: rule({ cache: 'contextual' })((parent, args, context) => {
+    const userId = getUserId(context);
+    return Boolean(userId);
+  }),
 };
 
 export const permissions = shield({
   Query: {
-    '*': allow,
-    me: rules.isAuthenticatedUser,
+    '*': deny,
+    tracker: rules.isAuthenticated,
+    trackers: rules.isAuthenticated,
+    me: rules.isAuthenticated,
   },
   Mutation: {
     '*': deny,
-    createTracker: rules.isAuthenticatedUser,
+    createTracker: rules.isAuthenticated,
   },
 });
