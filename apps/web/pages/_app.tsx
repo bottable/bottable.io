0111da56aa1152a4715f653383bec4710ff2b380
balta-z-r <zunigaruizb@gmail.com>
2021-02-05 15:34:14 +0800

    feat: dashboard (not done)

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
 import { UIProvider } from 'fiber-ui';
 import './styles.css';
 
@@ -10,18 +8,7 @@ require('typeface-work-sans');
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <UIProvider>
-      <Head>
-        <title>Welcome to web!</title>
-      </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to web!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <Component {...pageProps} />
     </UIProvider>
   );
 }
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,100 +1,117 @@
 import React from 'react';
-import { Button } from 'fiber-ui';
-
-import styles from './index.module.scss';
+import {
+  Button,
+  Heading,
+  Layout,
+  Text,
+  Input,
+  Space,
+  Card,
+  Tag,
+} from 'fiber-ui';
+import { MdWhatshot } from 'react-icons/md';
+import { FaLongArrowAltDown } from 'react-icons/fa';
+import { AiFillPushpin } from 'react-icons/ai';
 
 export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.scss file.
-   */
+  const { Search } = Input;
+
   return (
-    <div className={styles.page}>
-      <h2>Resources &amp; Tools</h2>
-      <Button>Click Here!</Button>
-      <p>Thank you for using and showing some ♥ for Nx.</p>
-      <div className="flex github-star-container">
-        <a
-          href="https://github.com/nrwl/nx"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {' '}
-          If you like Nx, please give it a star:
-          <div className="github-star-badge">
-            <img src="/star.svg" className="material-icons" alt="" />
-            Star
+    <Layout style={{ marginTop: 48 }}>
+      <Layout.Sider>
+        <Heading>Bottable</Heading>
+      </Layout.Sider>
+      <Layout>
+        <Layout.Content style={{ background: '#f7f7f7' }}>
+          <div style={{ display: 'flex' }}>
+            <Heading>Hello Kevin,</Heading>
+            <Button type="primary" style={{ alignSelf: 'flex-end' }}>
+              See All Trackers
+            </Button>
           </div>
-        </a>
-      </div>
-      <p>Here are some links to help you get started.</p>
-      <ul className="resources">
-        <li className="col-span-2">
-          <a
-            className="resource flex"
-            href="https://egghead.io/playlists/scale-react-development-with-nx-4038"
-          >
-            Scale React Development with Nx (Course)
-          </a>
-        </li>
-        <li className="col-span-2">
-          <a
-            className="resource flex"
-            href="https://nx.dev/latest/react/tutorial/01-create-application"
-          >
-            Interactive tutorial
-          </a>
-        </li>
-        <li className="col-span-2">
-          <a className="resource flex" href="https://nx.app/">
-            <svg
-              width="36"
-              height="36"
-              viewBox="0 0 120 120"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M120 15V30C103.44 30 90 43.44 90 60C90 76.56 76.56 90 60 90C43.44 90 30 103.44 30 120H15C6.72 120 0 113.28 0 105V15C0 6.72 6.72 0 15 0H105C113.28 0 120 6.72 120 15Z"
-                fill="#0E2039"
-              />
-              <path
-                d="M120 30V105C120 113.28 113.28 120 105 120H30C30 103.44 43.44 90 60 90C76.56 90 90 76.56 90 60C90 43.44 103.44 30 120 30Z"
-                fill="white"
-              />
-            </svg>
-            <span className="gutter-left">Nx Cloud</span>
-          </a>
-        </li>
-      </ul>
-      <h2>Next Steps</h2>
-      <p>Here are some things you can do with Nx.</p>
-      <details open>
-        <summary>Add UI library</summary>
-        <pre>{`# Generate UI lib
-nx g @nrwl/react:lib ui
-
-# Add a component
-nx g @nrwl/react:component xyz --project ui`}</pre>
-      </details>
-      <details>
-        <summary>View dependency graph</summary>
-        <pre>{`nx dep-graph`}</pre>
-      </details>
-      <details>
-        <summary>Run affected commands</summary>
-        <pre>{`# see what's been affected by changes
-nx affected:dep-graph
-
-# run tests for current changes
-nx affected:test
-
-# run e2e tests for current changes
-nx affected:e2e
-`}</pre>
-      </details>
-    </div>
+          <Text>We've done the work for you. Check it out.</Text>
+          <Input.Search
+            style={{ width: '100%' }}
+            placeholder="Search for any tracker or category"
+          />
+          <Space>
+            <MdWhatshot />
+            <Heading size={2}>Hot</Heading>
+          </Space>
+          <Text>Everything that changed</Text>
+          <Space>
+            <Card bordered={false} title="Amazon Nike React 4" width={300}>
+              <Text strong style={{ color: '#929292' }}>
+                Price
+              </Text>
+              <div
+                style={{
+                  background: '#eee',
+                  textAlign: 'center',
+                  padding: '10px 0px',
+                }}
+              >
+                <Text>5103</Text>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <FaLongArrowAltDown />
+              </div>
+              <div
+                style={{
+                  background: '#eee',
+                  textAlign: 'center',
+                  padding: '10px 0px',
+                }}
+              >
+                <Text strong>4500</Text>
+              </div>
+            </Card>
+          </Space>
+          <Space>
+            <AiFillPushpin />
+            <Heading size={2}>Pinned</Heading>
+          </Space>
+          <Text>All your personal favorites</Text>
+          <Space direction="vertical">
+            <Space>
+              <Card width={300} title="Amazon Nike React 4" bordered={false}>
+                <Tag>Tag 1</Tag>
+                <Tag>Tag 2</Tag>
+                <Tag>Tag 3</Tag>
+              </Card>
+              <Card width={300} title="Tracker Name" bordered={false}>
+                <Tag>Tag 1</Tag>
+                <Tag>Tag 2</Tag>
+                <Tag>Tag 3</Tag>
+              </Card>
+              <Card width={300} title="Tracker Name" bordered={false}>
+                <Tag>Tag 1</Tag>
+                <Tag>Tag 2</Tag>
+                <Tag>Tag 3</Tag>
+              </Card>
+            </Space>
+            <Space>
+              <Card width={300} title="Tracker Name" bordered={false}>
+                <Tag>Tag 1</Tag>
+                <Tag>Tag 2</Tag>
+                <Tag>Tag 3</Tag>
+              </Card>
+              <Card width={300} title="Tracker Name" bordered={false}>
+                <Tag>Tag 1</Tag>
+                <Tag>Tag 2</Tag>
+                <Tag>Tag 3</Tag>
+              </Card>
+              <Card width={300} title="Tracker Name" bordered={false}>
+                <Tag>Tag 1</Tag>
+                <Tag>Tag 2</Tag>
+                <Tag>Tag 3</Tag>
+              </Card>
+            </Space>
+          </Space>
+        </Layout.Content>
+        <Layout.Footer>Footer</Layout.Footer>
+      </Layout>
+    </Layout>
   );
 }
 
