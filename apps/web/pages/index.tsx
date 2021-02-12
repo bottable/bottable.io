8fbffd15a1586132a8e79b8825f60816d074bd84
balta-z-r <zunigaruizb@gmail.com>
2021-02-13 00:51:47 +0800

    feat: dashboard main page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -17,33 +17,58 @@ export function Index() {
   const { Search } = Input;
 
   return (
-    <Layout style={{ marginTop: 48 }}>
+    <Layout>
       <Layout.Sider>
         <Heading>Bottable</Heading>
       </Layout.Sider>
       <Layout>
-        <Layout.Content style={{ background: '#f7f7f7' }}>
-          <div style={{ display: 'flex' }}>
+        <Layout.Content
+          style={{ background: '#f7f7f7', padding: '30px 70px 90px 70px' }}
+        >
+          <div style={{ display: 'flex', alignItems: 'center' }}>
             <Heading>Hello Kevin,</Heading>
-            <Button type="primary" style={{ alignSelf: 'flex-end' }}>
+            <Button type="primary" style={{ marginLeft: 'auto' }}>
               See All Trackers
             </Button>
           </div>
-          <Text>We've done the work for you. Check it out.</Text>
+          <div style={{ marginBottom: 20 }}>
+            <Text>We've done the work for you. Check it out.</Text>
+          </div>
           <Input.Search
-            style={{ width: '100%' }}
+            style={{
+              width: '100%',
+              boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.2)',
+              padding: '16px 20px',
+            }}
             placeholder="Search for any tracker or category"
+            bordered={false}
           />
-          <Space>
-            <MdWhatshot />
-            <Heading size={2}>Hot</Heading>
-          </Space>
-          <Text>Everything that changed</Text>
-          <Space>
-            <Card bordered={false} title="Amazon Nike React 4" width={300}>
-              <Text strong style={{ color: '#929292' }}>
-                Price
-              </Text>
+          <div>
+            <Space size="sm">
+              <MdWhatshot size={40} />
+              <Heading size={2}>Hot</Heading>
+            </Space>
+          </div>
+          <div style={{ marginBottom: 20 }}>
+            <Text>Everything that changed</Text>
+          </div>
+          <div>
+            <Card
+              bordered={false}
+              width={350}
+              style={{
+                color: 'black',
+                boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.2)',
+              }}
+            >
+              <div>
+                <Text strong>Amazon Nike React 4</Text>
+              </div>
+              <div style={{ marginBottom: 12 }}>
+                <Text strong style={{ color: '#929292' }}>
+                  Price
+                </Text>
+              </div>
               <div
                 style={{
                   background: '#eee',
@@ -66,48 +91,96 @@ export function Index() {
                 <Text strong>4500</Text>
               </div>
             </Card>
-          </Space>
-          <Space>
-            <AiFillPushpin />
-            <Heading size={2}>Pinned</Heading>
-          </Space>
-          <Text>All your personal favorites</Text>
-          <Space direction="vertical">
-            <Space>
-              <Card width={300} title="Amazon Nike React 4" bordered={false}>
-                <Tag>Tag 1</Tag>
-                <Tag>Tag 2</Tag>
-                <Tag>Tag 3</Tag>
-              </Card>
-              <Card width={300} title="Tracker Name" bordered={false}>
-                <Tag>Tag 1</Tag>
-                <Tag>Tag 2</Tag>
-                <Tag>Tag 3</Tag>
-              </Card>
-              <Card width={300} title="Tracker Name" bordered={false}>
-                <Tag>Tag 1</Tag>
-                <Tag>Tag 2</Tag>
-                <Tag>Tag 3</Tag>
-              </Card>
+          </div>
+          <div>
+            <Space size="sm">
+              <AiFillPushpin size={40} />
+              <Heading size={2}>Pinned</Heading>
             </Space>
-            <Space>
-              <Card width={300} title="Tracker Name" bordered={false}>
-                <Tag>Tag 1</Tag>
-                <Tag>Tag 2</Tag>
-                <Tag>Tag 3</Tag>
-              </Card>
-              <Card width={300} title="Tracker Name" bordered={false}>
-                <Tag>Tag 1</Tag>
-                <Tag>Tag 2</Tag>
-                <Tag>Tag 3</Tag>
-              </Card>
-              <Card width={300} title="Tracker Name" bordered={false}>
-                <Tag>Tag 1</Tag>
-                <Tag>Tag 2</Tag>
-                <Tag>Tag 3</Tag>
-              </Card>
-            </Space>
-          </Space>
+          </div>
+          <div style={{ marginBottom: 20 }}>
+            <Text>All your personal favorites</Text>
+          </div>
+          <div style={{ display: 'flex', width: '100%', flexWrap: 'wrap' }}>
+            <Card
+              width={350}
+              style={{
+                margin: '12px 16px 12px 0px',
+                boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.2)',
+              }}
+              title="Amazon Nike React 4"
+              bordered={false}
+            >
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 1</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 2</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 3</Tag>
+            </Card>
+            <Card
+              width={350}
+              style={{
+                margin: '12px 16px 12px 0px',
+                boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.2)',
+              }}
+              title="Tracker Name"
+              bordered={false}
+            >
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 1</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 2</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 3</Tag>
+            </Card>
+            <Card
+              width={350}
+              style={{
+                margin: '12px 16px 12px 0px',
+                boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.2)',
+              }}
+              title="Tracker Name"
+              bordered={false}
+            >
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 1</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 2</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 3</Tag>
+            </Card>
+            <Card
+              width={350}
+              style={{
+                margin: '12px 16px 12px 0px',
+                boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.2)',
+              }}
+              title="Tracker Name"
+              bordered={false}
+            >
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 1</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 2</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 3</Tag>
+            </Card>
+            <Card
+              width={350}
+              style={{
+                margin: '12px 16px 12px 0px',
+                boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.2)',
+              }}
+              title="Tracker Name"
+              bordered={false}
+            >
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 1</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 2</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 3</Tag>
+            </Card>
+            <Card
+              width={350}
+              style={{
+                margin: '12px 16px 12px 0px',
+                boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.2)',
+              }}
+              title="Tracker Name"
+              bordered={false}
+            >
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 1</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 2</Tag>
+              <Tag style={{ marginBottom: '7px', marginTop: '7px' }}>Tag 3</Tag>
+            </Card>
+          </div>
         </Layout.Content>
         <Layout.Footer>Footer</Layout.Footer>
       </Layout>
